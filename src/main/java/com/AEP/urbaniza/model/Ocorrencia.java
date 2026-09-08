38d453b11bc691e7640034356ec46601d17c823a
Guilherme Luvizutto <luvizutto28@gmail.com>
2026-09-08 17:17:09 -0300

    Adicionado entidade ocorrencia

---
+++ src/main/java/com/AEP/urbaniza/model/Ocorrencia.java
@@ -0,0 +1,15 @@
+package com.AEP.urbaniza.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "ocorrencias")
+public class Ocorrencia {
+
+    @Id
+    private String id;
+    private String descricao;
+    private String categoria;
+    private String status;
+
+}
