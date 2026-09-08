3a28c644170f7aae9007c4030ff4655fb016e9c4
Guilherme Luvizutto <luvizutto28@gmail.com>
2026-09-08 17:04:27 -0300

    Inicializando projeto Spring Boot

---
+++ src/main/java/com/AEP/urbaniza/UrbanizaApplication.java
@@ -0,0 +1,13 @@
+package com.AEP.urbaniza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanizaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AEP/urbaniza/UrbanizaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AEP.urbaniza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
