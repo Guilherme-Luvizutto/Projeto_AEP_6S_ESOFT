27f97c4b6fb0580587be0e2aa53805f9fdfa7fc7
Guilherme Luvizutto <luvizutto28@gmail.com>
2026-09-09 17:17:18 -0300

    Adicionado endpoints CRUD de ocorrencias

--- src/main/java/com/AEP/urbaniza/controller/OcorrenciaController.java
+++ src/main/java/com/AEP/urbaniza/controller/OcorrenciaController.java
@@ -2,9 +2,9 @@ package com.AEP.urbaniza.controller;
 
 import com.AEP.urbaniza.model.Ocorrencia;
 import com.AEP.urbaniza.service.OcorrenciaService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class OcorrenciaController {
@@ -20,4 +20,23 @@ public class OcorrenciaController {
         return service.criar(ocorrencia);
     }
 
+    @GetMapping("/ocorrencias")
+    public List<Ocorrencia> listarTodos(){
+        return service.listarTodos();
+    }
+
+    @GetMapping("/ocorrencias/{id}")
+    public Ocorrencia buscarPorId(@PathVariable String id){
+        return service.buscarPorId(id);
+    }
+
+    @PutMapping("/ocorrencias/{id}")
+    public Ocorrencia atualizar(@PathVariable String id, @RequestBody Ocorrencia ocorrencia){
+        return service.atualizar(id, ocorrencia);
+    }
+
+    @DeleteMapping("/ocorrencias/{id}")
+    public void deletar(@PathVariable String id) {
+        service.deletar(id);
+    }
 }
